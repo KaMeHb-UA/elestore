2c598be9aabb69c42372e23eef57b68869e767f0
KaMeHb-UA <blade.whiteblack@gmail.com>
2018-02-08 20:16:53 +0200

    Updated to 0.0.5 specification, implemented all the methods (beta)

--- plugins/sources/pseudo-pkgs.js
+++ plugins/sources/pseudo-pkgs.js
@@ -2,7 +2,7 @@
  * @typedef {Object} ListParameters
  * @property {Array<String>} categories Apps category
  * @property {Array<number>} ratings Apps ratings
- * @property {Boolean} installed Show only installed apps
+ * @property {Boolean} installed Show only instaled apps
  */
 /**
  * @typedef {Object} App
@@ -19,6 +19,19 @@
  * @typedef {Object} Image
  * @property {String} thumbnail Link to image
  * @property {String} fullSize Link to image
+ */
+/**
+ * @typedef {Object} ReviewProperties
+ * @property {String} lang Review lang
+ * @property {number} rate Rate for app
+ */
+/**
+ * @typedef {Object} Review
+ * @property {number} rating Rating attached to review
+ * @property {String} uname User name
+ * @property {String} summary Review title
+ * @property {String} text Review text
+ * @property {String} date Date of publish
  */
 
 var packages = [
@@ -129,6 +142,23 @@ var packages = [
         'pseudopkg-8': 4.90324,
         'pseudopkg-9': 4.11204
     },
+    ratingsCount = {
+        'pseudopkg-0': 679,
+        'pseudopkg-1': 1563,
+        'pseudopkg-2': 854,
+        'pseudopkg-3': 435,
+        'pseudopkg-4': 439,
+        'pseudopkg-5': 501,
+        'pseudopkg-6': 6573,
+        'pseudopkg-7': 344,
+        'pseudopkg-8': 127,
+        'pseudopkg-9': 198
+    },
+    ratedByMe = {
+        'pseudopkg-0': 4,
+        'pseudopkg-1': 2,
+        'pseudopkg-8': 1
+    },
     installed = [
         'pseudopkg-0',
         'pseudopkg-1',
@@ -164,6 +194,48 @@ var packages = [
         'pseudopkg-0': 'pseudoPackage 0',
         'pseudopkg-2': 'pseudoPackage 2',
         'pseudopkg-6': 'pseudoPackage 6',
+    },
+    spaces = {
+        'pseudopkg-0': {
+            toDownload: 8546146,
+            toInstall: 23434534
+        },
+        'pseudopkg-1': {
+            toDownload: 8546146,
+            toInstall: 23434534
+        },
+        'pseudopkg-2': {
+            toDownload: 8546146,
+            toInstall: 23434534
+        },
+        'pseudopkg-3': {
+            toDownload: 8546146,
+            toInstall: 23434534
+        },
+        'pseudopkg-4': {
+            toDownload: 8546146,
+            toInstall: 23434534
+        },
+        'pseudopkg-5': {
+            toDownload: 8546146,
+            toInstall: 23434534
+        },
+        'pseudopkg-6': {
+            toDownload: 8546146,
+            toInstall: 23434534
+        },
+        'pseudopkg-7': {
+            toDownload: 8546146,
+            toInstall: 23434534
+        },
+        'pseudopkg-8': {
+            toDownload: 8546146,
+            toInstall: 23434534
+        },
+        'pseudopkg-9': {
+            toDownload: 8546146,
+            toInstall: 23434534
+        },
     },
     cache = {};
 module.exports = (Interface=>{
@@ -214,6 +286,15 @@ module.exports = (Interface=>{
         for(let i in displayNames){
             cache.apps[i].displayName = displayNames[i]
         }
+        for(let i in spaces){
+            cache.apps[i].neededSpace = spaces[i]
+        }
+        for(let i in ratingsCount){
+            cache.apps[i].ratingsCount = ratingsCount[i]
+        }
+        for(let i in ratedByMe){
+            cache.apps[i].myRating = ratedByMe[i]
+        }
     }
     /**
      * Lists all avaiable apps
@@ -242,7 +323,7 @@ module.exports = (Interface=>{
                 version: cache.apps[i].version
             })
         }
-        callback(null, appsDB);
+        callback(null, appsDB)
     }
     /**
      * Gets links for avaiable images for app (jpg, png, gif, svg, etc. (all supported by Chromium))
@@ -251,7 +332,13 @@ module.exports = (Interface=>{
      * @return {Void}
      */
     images(name, callback){
-        // do some stuff
+        var tmp = [];
+        cache.apps[name].images.forEach(img => {
+            tmp.push({
+                thumbnail: img.thumb,
+                fullSize: img.src
+            })
+        }); callback(null, tmp)
     }
     /**
      * Gets needed space for app and dependices
@@ -260,7 +347,7 @@ module.exports = (Interface=>{
      * @return {Void}
      */
     space(name, callback){ // collect with dependices!!!
-        // do some stuff
+        callback(null, cache.apps[name].neededSpace)
     }
     /**
      * Rates app
@@ -270,28 +357,44 @@ module.exports = (Interface=>{
      * @return {Void}
      */
     rate(name, value, callback){
-        // do some stuff
+        (
+            cache.apps[name].myRating && (
+                callback(new Error('Cannot rate twice')),
+                true
+            )
+        ) || (
+            cache.apps[name].rating = ((cache.apps[name].rating * cache.apps[name].ratingsCount++) + value) / cache.apps[name].ratingsCount,
+            cache.apps[name].myRating = value,
+            callback(null)
+        )
     }
     /**
      * Gets app's rating
      * @param {String} name App name
-     * @param {number} value "Stars" count (1-5)
-     * @param {function(Error):void} callback Standard NodeJS callback
+     * @param {function(Error, number):void} callback Standard NodeJS callback
      * @return {Void}
      */
     rating(name, callback){
-        // do some stuff
+        callback(null, cache.apps[name].rating)
     }
     /**
      * Adds a review to app
      * @param {String} name App name
      * @param {ReviewProperties} props Review's properties
      * @param {String} review Review text
+     * @param {String} reviewShort Short review text (heading)
      * @param {function(Error):void} callback Standard NodeJS callback
      * @return {Void}
      */
-    review(name, props, review, callback){
-        // do some stuff
+    review(name, props, review, reviewShort, callback){
+        cache.apps[name].reviews.this[props.lang] || (cache.apps[name].reviews.this[props.lang] = []);
+        cache.apps[name].reviews.this[props.lang].push({
+            text: review,
+            user: cache.me.uname,
+            summary: reviewShort,
+            rating: props.rate
+        });
+        this.rate(name, props.rate, callback)
     }
     /**
      * Gets app's reviews
@@ -302,6 +405,12 @@ module.exports = (Interface=>{
      */
     reviews(name, langs, callback){
         !callback && (typeof langs == 'function') && (callback = langs, langs = false) // if langs is not defined
-        // do some stuff
+        var tmp = [];
+        for(var lang in cache.apps[name].reviews.this){
+            ((langs && (langs.indexOf(lang) + 1)) || !langs) && (
+                cache.apps[name].reviews.this[lang].forEach(review => {tmp.push(review)})
+            )
+        }
+        callback(null, tmp)
     }
 })
