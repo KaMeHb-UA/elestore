43a7954d3bbb5b4080cee5336ec4e84c28309695
KaMeHb-UA <marlock@etlgr.com>
2018-02-20 05:26:34 +0200

    Dependencies auto-install in production done! Yahooo😄

--- main.js
+++ main.js
@@ -9,14 +9,45 @@ const {app, BrowserWindow} = require('electron'),
     copydir = require('copy-dir'),
     pkg = require(path.join(__dirname, 'package.json')),
     child_process = require('child_process'),
-    __appdir = path.resolve(require('homedir')(), '.config/elestore/3rd-party');
+    __appdir = path.resolve(require('homedir')(), '.config/elestore/3rd-party'),
+    npm = require('npm');
+
+function npm_install(dir, logger){
+    return new Promise(function(resolve, reject){
+        var previous = process.cwd();
+        process.chdir(dir);
+        npm.load({
+            'bin-links': false,
+            verbose: true,
+            prefix: dir
+        }, (err) => {
+            // handle errors
+            if(err) {
+                return reject(err);
+            }
+            // install modules
+            npm.commands.install([], (err, data) => {
+                process.chdir(previous);
+                if(err) {
+                    reject(err);
+                }
+                // log errors or data
+                resolve(data);
+            });
+            npm.on('log', (message) => {
+                // log installation progress
+                logger(message);
+            });
+        });
+    })
+}
 
 try{
     var settings = require(path.join(__appdir, 'settings.json'));
 } catch(e){
     mkdirpSync(__appdir);
     copydir.sync(path.join(__dirname, '3rd-party'), __appdir);
-    child_process.spawn(path.resolve(__dirname, '../../elestore'), [], {
+    child_process.spawn(process.argv[0], [], {
         detached: true
     });
     process.exit()
@@ -28,35 +59,35 @@ let win
 
 function loadPage(name){
     return win.loadURL(url.format({
-		pathname: path.join(__appdir, 'themes', settings.theme, `${name}.html`),
-		protocol: 'file:',
-		slashes: true
-	}), {
+        pathname: path.join(__appdir, 'themes', settings.theme, `${name}.html`),
+        protocol: 'file:',
+        slashes: true
+    }), {
         API: require(path.join(__dirname, 'plugins/api.js'))
     })
 }
 
 function createWindow () {
-	// Create the browser window.
-	win = new BrowserWindow({
+    // Create the browser window.
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         minHeight: 400,
         minWidth: 500
     })
 
-	// and load the startup page of the app.
+    // and load the startup page of the app.
     loadPage('startup');
 
-	// Open the DevTools.
-	win.webContents.openDevTools()
+    // Open the DevTools.
+    win.webContents.openDevTools()
 
-	// Emitted when the window is closed.
-	win.on('closed', () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		win = null
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
     });
 
     win.setMenu(null)
@@ -69,20 +100,28 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-	// On macOS it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-	// On macOS it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (win === null) {
-		createWindow()
-	}
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
 })
+
+function waitForWebContents(callback){
+    if(!win || !win.webContents) setTimeout(()=>{waitForWebContents(callback)}, 100); else callback();
+}
+
+function execJS(text){
+    waitForWebContents(()=>{win.webContents.executeJavaScript(text)})
+}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
@@ -91,12 +130,42 @@ var pluginsPath = path.join(__appdir, 'plugins/sources');
 
 fs.readdir(pluginsPath, (err, files) => {
     if (err) return console.log(err);
-    var sourceInterfaces = [];
+    var sourceInterfaces = [], toInstall = [], depsUpdated = false, internalErr = false;
     files.forEach(file => {
-        if (file != '.' && file != '..' && file != 'source-sample.js' && file != 'source-sample') sourceInterfaces.push(require(path.join(pluginsPath, file)));
+        if (file != '.' && file != '..' && file != 'source-sample'){
+            if(!fs.existsSync(path.join(pluginsPath, file, 'node_modules'))){
+                depsUpdated = true;
+                toInstall.push(path.join(pluginsPath, file));
+            } else if (!toInstall.length) sourceInterfaces.push(require(path.join(pluginsPath, file)));
+        }
     });
-    global.API = new (require(path.join(__dirname, 'plugins/api.js')))(sourceInterfaces);
-    drawInterface();
+    if (toInstall.length){
+        execJS('setLoadingStage("Installing new dependencies")');
+        execJS(`internalConsole.log('Executed npm install')`);
+        execJS(`internalConsole.warn('Note: may not work in production')`);
+        (function a(dir){
+            if (dir) npm_install(dir, message => {
+                execJS(`internalConsole.log(${JSON.stringify(message)})`);
+            }).then(message => {
+                execJS(`internalConsole.log(${JSON.stringify(message)})`);
+                a(toInstall.shift())
+            }).catch(err => {
+                execJS(`internalConsole.err(${JSON.stringify(err.toString())})`);
+            }); else {
+                let tm = 3000;
+                execJS(`internalConsole.log("All done, restart in ${tm/1000} seconds")`)
+                setTimeout(() => {
+                    child_process.spawn(process.argv[0], [], {
+                        detached: true
+                    });
+                    process.exit()
+                }, tm)
+            }
+        })(toInstall.shift())
+    } else {
+        global.API = new (require(path.join(__dirname, 'plugins/api.js')))(sourceInterfaces);
+        drawInterface();
+    }
 });
 function drawInterface(){
     try{
